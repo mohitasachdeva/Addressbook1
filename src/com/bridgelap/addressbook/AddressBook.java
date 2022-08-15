97f66219ce17ad44169e1e33ec8deacda852d845
HP <sachdevamohita5@gmail.com>
2022-08-15 22:20:53 +0530

    add multiple contacts

--- src/com/bridgelap/addressbook/AddressBook.java
+++ src/com/bridgelap/addressbook/AddressBook.java
@@ -1,13 +1,15 @@
 package com.bridgelap.addressbook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    Contacts contact = new Contacts();
+   // Contacts contact = new Contacts();
+   ArrayList<Contacts> list = new ArrayList<>();
     Scanner scr = new Scanner(System.in);
 
     void addContact(){
-
+        Contacts contact = new Contacts();
         System.out.println("Enter first name");
         contact.setFirstName(scr.next());
         System.out.println("Enter last name");
@@ -23,60 +25,76 @@ public class AddressBook {
         contact.setPhoneNumber(scr.nextLong());
         System.out.println("Enter email address");
         contact.setEmail(scr.next());
+        list.add(contact);
 
     }
-    void editContact(){
+    void editContact() {
         System.out.println("Enter the first name of person to edit");
-        String firstName = scr.next();
-        if(firstName.equals(contact.getFirstName())){
-            System.out.println("Edit the details of person");
-            System.out.println("Enter first name");
-            contact.setFirstName(scr.next());
+        String firstName = scr.next().toLowerCase();
+        boolean found = false;
+        for (Contacts contact : list) {
+            if (firstName.equals(contact.getFirstName().toLowerCase())) {
+                System.out.println("Edit the details of person");
+                System.out.println("Enter first name");
+                contact.setFirstName(scr.next());
 
-            System.out.println("Enter last Name");
-            contact.setLastName(scr.next());
+                System.out.println("Enter last Name");
+                contact.setLastName(scr.next());
 
-            System.out.println("Enter Email");
-            contact.setEmail(scr.next());
+                System.out.println("Enter Email");
+                contact.setEmail(scr.next());
 
-            System.out.print("Enter address:  ");
-            scr.nextLine();
-            contact.setAddress(scr.nextLine());
+                System.out.print("Enter address:  ");
+                scr.nextLine();
+                contact.setAddress(scr.nextLine());
 
-            System.out.println("Enter phone number");
-            contact.setPhoneNumber(scr.nextLong());
+                System.out.println("Enter phone number");
+                contact.setPhoneNumber(scr.nextLong());
 
-            System.out.println("Enter state");
-            contact.setState(scr.next());
+                System.out.println("Enter state");
+                contact.setState(scr.next());
 
-            System.out.println("Enter city");
-            contact.setCity(scr.next());
+                System.out.println("Enter city");
+                contact.setCity(scr.next());
 
 
+            } else {
+                System.out.println("Person not found");
+            }
 
         }
-        else
-        {
-            System.out.println("Person not found");
-        }
-
     }
     void deleteContact(){
         System.out.println("Enter the first name of person to edit");
-        String firstName = scr.next();
-
-        if (firstName.equals(contact.getFirstName())){
-            contact=null;
+        String firstName = scr.next().toLowerCase();
+            boolean found = false;
+            for (Contacts contact : list){
+        if (firstName.equals(contact.getFirstName().toLowerCase())){
+            list.remove(contact);
+            found = true;
+            System.out.println("Contact deleted successfully");
+            break;
         }
-        else{
-            System.out.println("no contact found");
+            }
+            if (!found){
+                System.out.println("No contact found");
+            }
         }
 
-    }
+
 
     Contacts display(){
-        return contact;
+        if(list.isEmpty()){
+            System.out.println("No contacts to display");
+        }
+        else {
+            for (Contacts contact : list) {
+                System.out.println(contact);
+            }
+        }
     }
 }
+
+
 
 
